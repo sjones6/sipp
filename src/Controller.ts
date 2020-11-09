0b37285a046998b8be3f26b2bf4b230a0d66a901
sjones6 <jones.spencera@gmail.com>
2020-11-08 22:27:03 -0500

    expose facades as controller props

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,10 +1,16 @@
-import { Response } from 'express'
 import { BaseException } from './exceptions';
 import { IHTTPResponseFacade, IRequestContext } from './interfaces';
+import { view, json, text, redirect } from './facades'
 
 export class Controller {
 
   public readonly basePath: string | null = null;
+
+  /* expose response facades as properties */
+  protected readonly view = view;
+  protected readonly json = json;
+  protected readonly text = text;
+  protected readonly redirect = redirect;
 
   public getBasePath(): string {
     const standardize = (path) => path.replace(/([A-Z])/g, ' $1') // replace all caps with space before
